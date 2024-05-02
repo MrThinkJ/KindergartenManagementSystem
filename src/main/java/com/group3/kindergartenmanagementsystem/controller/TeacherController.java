8acb6a380fdf2a824e0086c0d018345d03cb64dc
MrThinkJ <ledinhthinh.ws@gmail.com>
2024-05-02 14:56:36 +0700

    Add get all teacher api

--- src/main/java/com/group3/kindergartenmanagementsystem/controller/TeacherController.java
+++ src/main/java/com/group3/kindergartenmanagementsystem/controller/TeacherController.java
@@ -1,19 +1,24 @@
 package com.group3.kindergartenmanagementsystem.controller;
 
+import com.group3.kindergartenmanagementsystem.payload.UserDTO;
 import com.group3.kindergartenmanagementsystem.service.TeacherService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/teacher")
 public class TeacherController {
     TeacherService teacherService;
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserDTO>> getAllTeacher(){
+        return ResponseEntity.ok(teacherService.getAllTeacher());
+    }
     @PutMapping("/{teacherId}/addToClass/{classroomId}")
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> addTeacherToClass(@PathVariable Integer teacherId,
